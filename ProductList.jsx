de0f060c2f42ecc0376d41a1750c034c6b83d640
KonthamDivyateja <139646440+KonthamDivyateja@users.noreply.github.com>
2025-12-30 23:40:12 +0530

    Add files via upload

---
+++ ProductList.jsx
@@ -0,0 +1,19 @@
+export default function ProductList({ products, onSelect }) {
+  console.log("ProductList rendered");
+
+  return (
+    <div>
+      <h3>Product List</h3>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ₹{product.price}
+            <button onClick={() => onSelect(product)}>
+              Select
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
